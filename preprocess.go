441c367062f519c0947ec80a72c9ff99cfe3a5c9
yetsing <heopro@163.com>
2025-06-19 21:41:59 +0800

    feat: implement conditional inclusion with #if and #endif directives in preprocessing

--- preprocess.go
+++ preprocess.go
@@ -5,6 +5,14 @@ import (
 	"path"
 )
 
+// `#if` can be nested, so we use a stack to manage nested `#if`s.
+type CondIncl struct {
+	next *CondIncl // Next condition
+	tok  *Token    // Token that starts this condition
+}
+
+var sCondIncl *CondIncl // Stack of conditional inclusions
+
 func isHash(tok *Token) bool {
 	return tok.atBol && tok.equal("#")
 }
@@ -13,6 +21,13 @@ func copyToken(tok *Token) *Token {
 	t := &Token{}
 	*t = *tok
 	t.next = nil
+	return t
+}
+
+func newEof(tok *Token) *Token {
+	t := copyToken(tok)
+	t.kind = TK_EOF
+	t.literal = ""
 	return t
 }
 
@@ -31,6 +46,59 @@ func appendToken(tok1, tok2 *Token) *Token {
 	}
 	cur.next = tok2
 	return head.next
+}
+
+// Skip until next `#endif`.
+func skipCondIncl(tok *Token) *Token {
+	for tok.kind != TK_EOF {
+		if isHash(tok) && tok.next.equal("endif") {
+			return tok
+		}
+		tok = tok.next
+	}
+	return tok
+}
+
+// Copy all tokens until the next newline, terminate them with
+// an EOF token and then returns them. This function is used to
+// create a new list of tokens for `#if` arguments.
+func copyLine(rest **Token, tok *Token) *Token {
+	head := Token{}
+	cur := &head
+
+	for ; !tok.atBol; tok = tok.next {
+		cur.next = copyToken(tok)
+		cur = cur.next
+	}
+	cur.next = newEof(tok)
+	*rest = tok
+	return head.next
+}
+
+// Read and evaluate a constant expression.
+func evalConstExpr(rest **Token, tok *Token) int64 {
+	start := tok
+	expr := copyLine(rest, tok.next)
+
+	if expr.kind == TK_EOF {
+		errorTok(start, "no expression")
+	}
+
+	gtok = expr
+	val := constExpr()
+	if gtok.kind != TK_EOF {
+		errorTok(gtok, "extra token")
+	}
+	return val
+}
+
+func pushCondIncl(tok *Token) *CondIncl {
+	ci := &CondIncl{
+		next: sCondIncl,
+		tok:  tok,
+	}
+	sCondIncl = ci
+	return ci
 }
 
 // Some preprocessor directives such as #include allow extraneous
@@ -61,6 +129,7 @@ func preprocess2(tok *Token) *Token {
 			continue
 		}
 
+		start := tok
 		tok = tok.next
 
 		if tok.equal("include") {
@@ -82,6 +151,24 @@ func preprocess2(tok *Token) *Token {
 			}
 			tok = skipLine(tok.next)
 			tok = appendToken(tok2, tok)
+			continue
+		}
+
+		if tok.equal("if") {
+			val := evalConstExpr(&tok, tok)
+			pushCondIncl(start)
+			if val == 0 {
+				tok = skipCondIncl(tok)
+			}
+			continue
+		}
+
+		if tok.equal("endif") {
+			if sCondIncl == nil {
+				errorTok(tok, "stray `#endif`")
+			}
+			sCondIncl = sCondIncl.next
+			tok = skipLine(tok.next)
 			continue
 		}
 

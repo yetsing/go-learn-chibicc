30c94a38655b40a7f1f66994ffc4fd77287f32c7
yetsing <heopro@163.com>
2025-07-20 17:46:41 +0800

    feat: implement stdatomic.h with atomic types and operations

--- preprocess.go
+++ preprocess.go
@@ -1197,7 +1197,6 @@ func initMacros() {
 	defineMacro("__SIZEOF_SIZE_T__", "8")
 	defineMacro("__SIZE_TYPE__", "unsigned long")
 	defineMacro("__STDC_HOSTED__", "1")
-	defineMacro("__STDC_NO_ATOMICS__", "1")
 	defineMacro("__STDC_NO_COMPLEX__", "1")
 	defineMacro("__STDC_UTF_16__", "1")
 	defineMacro("__STDC_UTF_32__", "1")
